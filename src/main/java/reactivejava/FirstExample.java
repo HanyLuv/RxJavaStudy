820bf0baf971c3b0da1de5dcca4274db5b3907bf
hanyluv <hanyluv.928@gmail.com>
2019-03-26 01:00:23 +0900

    p.55 - p.56

--- src/main/java/reactivejava/FirstExample.java
+++ src/main/java/reactivejava/FirstExample.java
@@ -1,15 +1,48 @@
 package reactivejava;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 import io.reactivex.Observable;
 
 
 public class FirstExample {
 	
 	public static void main(String[] args) {
-				Integer[] arr= {100,200,300};
-				Observable<Integer> source = Observable.fromArray(arr);
-				source.subscribe(System.out::println);
 		
-	}
+		//1. List에서 Observable만들기.
+		List<String> name = new ArrayList<String>();
+		name.add("HANY");
+		name.add("ANNA");
+		name.add("MONMANG");
+		
+		Observable<String> source = Observable.fromIterable(name);
+		source.subscribe(System.out::println);
 
+		
+		//2. Set에서 Observable 만들기
+		Set<String> cities = new HashSet<String>();
+		cities.add("Seoul!");
+		cities.add("London~!");
+		
+		Observable<String> citiesSource = Observable.fromIterable(cities);
+		citiesSource.subscribe(System.out::println);
+
+		// BlockingQueue에서 만들기
+		BlockingQueue<User>  queue = new ArrayBlockingQueue<User>(100); // 최대 대기행렬수 100
+		queue.add(new User("Lone"));
+		queue.add(new User("Roma"));
+		queue.add(new User("PAPAPAP"));
+		
+		Observable<User> observable = Observable.fromIterable(queue);
+		observable.subscribe(data->{
+			System.out.println(data.getName());
+		});
+
+	}
 }
