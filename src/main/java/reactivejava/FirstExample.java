0fe26c6278f5cc59bb3061783f450b16e9e3d131
hanyluv <hanyluv.928@gmail.com>
2019-03-26 00:38:00 +0900

    p.53

--- src/main/java/reactivejava/FirstExample.java
+++ src/main/java/reactivejava/FirstExample.java
@@ -1,37 +1,15 @@
 package reactivejava;
 
 import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
-import io.reactivex.functions.Consumer;
+
 
 public class FirstExample {
 	
-	public void emit() {
-		Observable.just("Hello!",3)
-		.subscribe(System.out::println);
-		
-	}
-	
 	public static void main(String[] args) {
-
-		Observable<Integer> source = Observable.create(
-				(ObservableEmitter<Integer> emitter) ->{
-					emitter.onNext(1);
-					emitter.onNext(2);
-					emitter.onNext(3);
-					emitter.onComplete();
-		}
-		);
+				Integer[] arr= {100,200,300};
+				Observable<Integer> source = Observable.fromArray(arr);
+				source.subscribe(System.out::println);
 		
-		source.subscribe(new Consumer<Integer>() {
-			@Override
-			public void accept(Integer t) throws Exception {
-				System.out.println(t);
-				
-			}
-		});
 	}
 
-	
-	
 }
