15dd9320e66de0aa41095d03e077af5a93c674f1
hanyluv <hanyluv.928@gmail.com>
2019-03-29 00:37:37 +0900

    p.79 예제대로 했는데 작동을안함.. OTL 공부진행후 해결해보려함 'ㅅ'

--- src/main/java/reactivejava/FirstExample.java
+++ src/main/java/reactivejava/FirstExample.java
@@ -1,21 +1,27 @@
 package reactivejava;
 
-import io.reactivex.subjects.AsyncSubject;
-import io.reactivex.subjects.BehaviorSubject;
-import io.reactivex.subjects.PublishSubject;
-import io.reactivex.subjects.ReplaySubject;
+import java.util.concurrent.TimeUnit;
+
+
+import io.reactivex.Observable;
+
+import io.reactivex.observables.ConnectableObservable;
 
 public class FirstExample {
 
 	public static void main(String[] args) {
-			
-	ReplaySubject<String> subject = ReplaySubject.create();
-//	subject.subscribe(data -> System.out.println("Subscribe #1 => "+data ));
-	subject.onNext("1");
-	subject.subscribe(data -> System.out.println("Subscribe #1 => "+data ));
-	subject.onNext("2");
-	subject.subscribe(data -> System.out.println("Subscribe #2 => "+data ));
-	subject.onNext("3");
-	subject.subscribe(data -> System.out.println("Subscribe #3 => "+data ));
+//		 System.out.println("Subscriber ");
+		String[] dt = {"1","3","5"};
+//		Observable<String> balls = Observable.interval(100L, TimeUnit.MILLISECONDS) //시간, 시간단위
+//		.map(Long::intValue)
+//		.map(i-> dt[i])
+//		.take(dt.length);
+		Observable<String> balls = Observable.fromArray(dt);
+		ConnectableObservable<String> source = balls.publish();
+		source.subscribe(data -> System.out.println("Subscriber #1 => "+data));
+		source.subscribe(data -> System.out.println("Subscriber #2 => "+data));
+		source.connect();
+		
+		
 	}
 }
