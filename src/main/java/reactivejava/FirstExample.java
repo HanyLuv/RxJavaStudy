21627431ccaf4058e11d44507d9296975554fd42
hanyluv <hanyluv.928@gmail.com>
2019-03-28 22:35:48 +0900

    p. 71

--- src/main/java/reactivejava/FirstExample.java
+++ src/main/java/reactivejava/FirstExample.java
@@ -5,13 +5,12 @@ import io.reactivex.subjects.AsyncSubject;
 public class FirstExample {
 
 	public static void main(String[] args) {
-		
 			AsyncSubject<String> subject = AsyncSubject.create();
 			subject.subscribe(data -> System.out.println("Subscriber #1 => "+data));
 			subject.onNext("hany");
-			subject.onNext("luv");
-			subject.subscribe(data -> System.out.println("Subscriber #2 => "+data));
 			subject.onNext("end");
-			subject.onComplete();	
+			subject.onComplete();	// Observable과 마찬가지로 호출 이후엔 onNext()를 무시한다.
+			subject.subscribe(data -> System.out.println("Subscriber #2 => "+data));
+			subject.subscribe(data -> System.out.println("Subscriber #3 => "+data));
 	}
 }
