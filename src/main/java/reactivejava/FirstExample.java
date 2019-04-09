f23261440ec3e22d9b7a6d8008015a0dd7611ca7
hanyluv <hanyluv.928@gmail.com>
2019-04-10 01:31:43 +0900

    p.89 - 90

--- src/main/java/reactivejava/FirstExample.java
+++ src/main/java/reactivejava/FirstExample.java
@@ -7,27 +7,15 @@ import io.reactivex.functions.Function;
 public class FirstExample {
 
 	public static void main(String[] args) {
+	
+			//flatMap() 결과값이 Observable 이므로 여러개의 데이터를 발행 할수있음 !!
 		
+		Function<String, Observable<String>> getDiamonds = ball -> Observable.just(ball+"<>",ball+"<>");
 		
-		//인자로 전달받는값, 반환값
-		Function<String, Integer> ballToIndex = ball-> {
-			switch (ball) {
-			case "RED":	return 1;
-			case "YELLOW":	return 2;
-			case "GREEN":	return 3;
-			case "BLUE":	return 5;
-			default: return -1;
-			}
-		}; //람다 표현의 몸체는 {}안에 있어야하나, 문장이 한개면 생략가능 'ㅅ')
-		
-		String[] balls = {"RED","YELLOW","GREEN","BLUE"};
-		Observable<Integer> source = Observable.fromArray(balls).map(ballToIndex);
-		
-		source.subscribe(new Consumer<Integer>() {
-		public void accept(Integer t) throws Exception {
-			System.out.println(t);
-		};
-		});
+		String[] balls = {"1","3","5"};
+//		Observable<String> source = Observable.fromArray(balls).flatMap(getDiamonds);
+		Observable<String> source = Observable.fromArray(balls).flatMap(ball -> Observable.just(ball+"<>",ball+"<>")); //인라인
+		source.subscribe(System.out::println);
 		
 		
 	}
