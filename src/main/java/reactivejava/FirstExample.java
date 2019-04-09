6f98f026a13ea82555750bc9b4e49546c79911ea
hanyluv <hanyluv.928@gmail.com>
2019-04-10 01:16:40 +0900

    p.88 map()함수는 내가 원하는 값을 어떤함수 (Funtion인터페이스 객체나 람다표현식) 에 넣는것이며 원하는 함수를 정의할수있느냐가 관건 'ㅅ')!!!

--- src/main/java/reactivejava/FirstExample.java
+++ src/main/java/reactivejava/FirstExample.java
@@ -9,13 +9,22 @@ public class FirstExample {
 	public static void main(String[] args) {
 		
 		
-		Function<String, String> getDiamond = ball-> ball +"<>";
+		//인자로 전달받는값, 반환값
+		Function<String, Integer> ballToIndex = ball-> {
+			switch (ball) {
+			case "RED":	return 1;
+			case "YELLOW":	return 2;
+			case "GREEN":	return 3;
+			case "BLUE":	return 5;
+			default: return -1;
+			}
+		}; //람다 표현의 몸체는 {}안에 있어야하나, 문장이 한개면 생략가능 'ㅅ')
 		
-		String[] balls = {"1","2","3","5"};
-		Observable<String> source = Observable.fromArray(balls).map(getDiamond);
+		String[] balls = {"RED","YELLOW","GREEN","BLUE"};
+		Observable<Integer> source = Observable.fromArray(balls).map(ballToIndex);
 		
-		source.subscribe(new Consumer<String>() {
-		public void accept(String t) throws Exception {
+		source.subscribe(new Consumer<Integer>() {
+		public void accept(Integer t) throws Exception {
 			System.out.println(t);
 		};
 		});
