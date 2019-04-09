dbd527ee633ffb7094bf81298ed4f6df3c2010a1
hanyluv <hanyluv.928@gmail.com>
2019-04-10 01:10:40 +0900

    p.87 funtion 인터페이스 분리

--- src/main/java/reactivejava/FirstExample.java
+++ src/main/java/reactivejava/FirstExample.java
@@ -2,14 +2,17 @@ package reactivejava;
 
 import io.reactivex.Observable;
 import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
 
 public class FirstExample {
 
 	public static void main(String[] args) {
 		
 		
+		Function<String, String> getDiamond = ball-> ball +"<>";
+		
 		String[] balls = {"1","2","3","5"};
-		Observable<String> source = Observable.fromArray(balls).map(ball-> ball +"<>");
+		Observable<String> source = Observable.fromArray(balls).map(getDiamond);
 		
 		source.subscribe(new Consumer<String>() {
 		public void accept(String t) throws Exception {
