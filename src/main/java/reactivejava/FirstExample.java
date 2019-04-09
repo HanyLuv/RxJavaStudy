52df419c087603e274d5c19d2e2acd3cd6b9380d
hanyluv <hanyluv.928@gmail.com>
2019-04-10 01:08:41 +0900

    p.86

--- src/main/java/reactivejava/FirstExample.java
+++ src/main/java/reactivejava/FirstExample.java
@@ -1,26 +1,21 @@
 package reactivejava;
 
-import java.util.concurrent.TimeUnit;
-
-
 import io.reactivex.Observable;
-
-import io.reactivex.observables.ConnectableObservable;
+import io.reactivex.functions.Consumer;
 
 public class FirstExample {
 
 	public static void main(String[] args) {
-//		 System.out.println("Subscriber ");
-		String[] dt = {"1","3","5"};
-//		Observable<String> balls = Observable.interval(100L, TimeUnit.MILLISECONDS) //시간, 시간단위
-//		.map(Long::intValue)
-//		.map(i-> dt[i])
-//		.take(dt.length);
-		Observable<String> balls = Observable.fromArray(dt);
-		ConnectableObservable<String> source = balls.publish();
-		source.subscribe(data -> System.out.println("Subscriber #1 => "+data));
-		source.subscribe(data -> System.out.println("Subscriber #2 => "+data));
-		source.connect();
+		
+		
+		String[] balls = {"1","2","3","5"};
+		Observable<String> source = Observable.fromArray(balls).map(ball-> ball +"<>");
+		
+		source.subscribe(new Consumer<String>() {
+		public void accept(String t) throws Exception {
+			System.out.println(t);
+		};
+		});
 		
 		
 	}
